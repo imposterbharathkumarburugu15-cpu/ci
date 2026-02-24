a102ac4de46abfed5c13b32eaf2ad99b4676f35f
BHARATH KUMAR <imposterbharathkumarburugu15@gmail.com>
2026-02-25 01:23:56 +0530

    Allow CORS from all origins

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from src.predict import predict_customer
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://ci-ml.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def home():
